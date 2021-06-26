8c46f019c2f735303f943dc5ff39b65d7eb8906d
Noah Belsito <nbelsito02@gmail.com>
2021-06-26 09:49:10 -0400

    annotating structure

---
+++ main.py
@@ -0,0 +1,22 @@
+# start function to start off the project
+# two rooms with items in each
+# items help you get to the next room
+# Room 1 is a room full of keys
+#  Randomly selected keys could allow you to open a door to the next room
+#  two doors available to continue
+#    a random key could potentially open either
+#    first door goes to a rocket ship to mars
+#    second door goes to an gorilla that befriends you and breaks the next door for you
+#      exit
+# Room 2 is a room full of cooking supplies and two unlocked doors
+#  First door goes to a kitchen and the Second door goes to a Diamond and an exit
+#    learn valuable cooking skills
+#    find a key hidden in the oven if you end up cooking
+#    Two other doors
+#       One door leads to an exit
+#       One door leads to a room full of water that floors the kitchen and drowns you
+
+def start():
+    print("You wake up alone in a dark room.")
+    print("You must find your way through the darkness.")
+    print("Press 'o' to search for the lights or 'd' to search for doors")
